9de6d9ba1c726f3dc1138d1abad1b43ad54ef74a
santigc6 <santigc6@users.noreply.github.com>
2018-02-11 00:58:40 +0100

    Working

--- P1/ej3.lisp
+++ P1/ej3.lisp
@@ -12,23 +12,31 @@
 (defun combine-lst-lst (lst1 lst2)
   (if(or (null lst1) (null lst2))
       (return-from combine-lst-lst nil))
-  (mapcan #'(lambda (x) (combine-elt-lst x lst2)) lst1)
-  )
+  (mapcan #'(lambda (x) (combine-elt-lst x lst2)) lst1))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;3.3
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun combine-lst-lsts (lst1 lsts)
-  (if (null (second lsts))
-      (return-from combine-lst-lsts
-        (mapcar #'(lambda (x) (append lst1 (list x))) (first lsts))))
-  (combine-lst-lsts (append lst1 (list (caar lsts))) (rest lsts)))
-
-(combine-lst-lsts '(1) '((1 2 3 4) (+ -) (a b c d)))
+  (cond
+   ((null lst1) nil)
+   ((null lsts) lst1)
+   ((and (rest lsts) (null (second lsts))) nil)
+   ((not (null (second lsts)))
+    (combine-lst-lsts
+     (mapcan
+       #'(lambda (x) 
+           (mapcar #'(lambda (y) (if (listp x)
+                                     (append x (list y))
+                                   (append (list x) (list y)))) 
+           (first lsts))) 
+       lst1) 
+     (rest lsts)))
+   (T (mapcan #'(lambda (x) (mapcar #'(lambda (y) (append x (list y))) (first lsts))) lst1))))
  
 (defun combine-list-of-lsts (lstolsts)
-  (if (eql (first lstolsts) nil) 
-      (return-from combine-list-of-lsts nil))
-  (mapcan #'(lambda (x) (combine-lst-lsts (list x) (rest lstolsts))) (first lstolsts)))
+  (if (null lstolsts)
+      (return-from combine-list-of-lsts (list nil)))
+  (combine-lst-lsts (mapcar #'(lambda (x) (list x)) (first lstolsts)) (rest lstolsts)))
 
-(combine-list-of-lsts '((a b c) (+ -) (1 2 3 4)))
+(combine-list-of-lsts '((a b c)))
