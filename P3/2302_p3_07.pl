647608913592a8a7ad0f8b800ba3378911d0826e
Adrián Fernández <adrian-fernandez-amador@users.noreply.github.com>
2018-04-06 10:06:52 +0200

    Ej 1

--- P3/2302_p3_07.pl
+++ P3/2302_p3_07.pl
@@ -1,4 +1,5 @@
 pertenece(X, [X|_]).
 pertenece(X, [_|Rs]) :- pertenece(X, Rs).
 
-pertenece_m(X, L).
+pertenece_m(X, [X|_]) :- X \= [_|_].
+pertenece_m(X, [F|Rs]) :- pertenece_m(X, F); pertenece_m(X, Rs).
