32e5dac02092d1de10fc8f2af2faf3e99cb5c286
Adrián Fernández <adrian-fernandez-amador@users.noreply.github.com>
2018-04-06 11:06:13 +0200

    Ej 2 y comienzo del 3

--- P3/2302_p3_07.pl
+++ P3/2302_p3_07.pl
@@ -1,5 +1,25 @@
+% Adrián Fernández
+% Santiago González-Carvajal
+
+
+% Ejercicio 1
 pertenece(X, [X|_]).
 pertenece(X, [_|Rs]) :- pertenece(X, Rs).
 
 pertenece_m(X, [X|_]) :- X \= [_|_].
 pertenece_m(X, [F|Rs]) :- pertenece_m(X, F); pertenece_m(X, Rs).
+
+
+% Ejercicio 2
+concatena([], L, L).
+concatena([X|L1], L2, [X|L3]) :- concatena(L1, L2, L3).
+
+invierte([], []).
+invierte([X|L], R) :- invierte(L, LI), concatena(LI, [X], R).
+
+
+% Ejercicio 3
+insert([X-P], [], [X-P]).
+insert([X-P], [F-Q|Rs], R) :- P<Q, insert([X-P], [Rs], R).
+
+
