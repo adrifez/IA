8f6ec4d283be3046575b2bc0ebd07337efdb90f9
Adrián Fernández <adrian-fernandez-amador@users.noreply.github.com>
2018-04-13 11:42:45 +0200

    Actualizacion build_tree

--- P3/2302_p3_07.pl
+++ P3/2302_p3_07.pl
@@ -41,6 +41,7 @@ sort_list([X-P|Rs], SL) :- sort_list(Rs, SLr), insert([X-P], SLr, SL).
 
 
 % Ejercicio 6
+build_tree([], nil).
 build_tree([X-_], tree(X, nil, nil)).
 build_tree([F, S|Rs], tree(1, Ti, Td)) :- Rs = [], build_tree([F], Ti), build_tree([S], Td).
 build_tree([F, S|Rs], tree(1, Ti, Td)) :- Rs \= [], build_tree([F], Ti), build_tree([S|Rs], Td).
